98ecbd2250fc32727a4e3be66717a394b16cfd59
mizou9494 <betarmizou@gmail.com>
2024-11-21 18:25:22 +0100

    The clone is in starter version

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Auth() {
+  return (
+    <div className='relative h-full w-full bg-[url("/images/logo.png")] bg-no-repeat bg-center bg-fixed bg-cover'> 
+      <div className='bg-black w-full h-full lg:bg-opacity-80'>
+
+      </div>
+    </div>
+  )
+}
